a5898c24dcadbcbda7cc2a73c1facc263b4d5645
Макей 😈 <imac_makey@iMac-Makej.local>
2023-11-05 16:10:35 +0300

    Fixed Version 1.3

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -8,50 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     // Сущность "Игра"
     var game: Game!
+    
     // Элементы на сцене
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
     
     // MARK: - Жизненный цикл
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Создаем экземпляр сущности "Игра"
-        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        // Создаем генератор случайных чисел
+        let generator = NumberGenerator(startValue: 1, endValue: 50)!
+        // Создаем сущность игра
+        game = Game(valueGenerator: generator, rounds: 5)
         // Обновляем данные о текущем значении загаданного числа
-        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
+        updateLabelWithSecretNumber(newText: String(game.currentRound.currentSecretValue))
     }
     
     // MARK: - Взаимодействие View - Model
+    
     // Проверка выбранного пользователем числа
     @IBAction func checkNumber() {
         // Высчитываем очки за раунд
-        game.calculateScore(with: Int(slider.value))
+        game.currentRound.calculateScore(with: Int(slider.value))
         // Проверяем, окончена ли игра
         if game.isGameEnded {
+            // Показываем окно с итогами
             showAlertWith(score: game.score)
-            // Начинаем игру заново
+            // Рестартуем игру
             game.restartGame()
-        }
-        else {
+        } else {
+            // Начинаем новый раунд игры
             game.startNewRound()
         }
-        
         // Обновляем данные о текущем значении загаданного числа
-        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
+        updateLabelWithSecretNumber(newText: String(game.currentRound.currentSecretValue))
     }
+    
     // MARK: - Обновление View
+    
     // Обновление текста загаданного числа
-    private func updateLabelWithSecretNumber(newText: String ) {
+    func updateLabelWithSecretNumber(newText: String ) {
         label.text = newText
     }
+    
     // Отображение всплывающего окна со счетом
-    private func showAlertWith(score: Int) {
+    private func showAlertWith( score: Int ) {
         let alert = UIAlertController(
             title: "Игра окончена",
-            message: "Вы заработали \(score) очков", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Начать заново", style:.default, handler: nil))
+            message: "Вы заработали \(score) очков",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
 }
